b6e0e7f307e8b0c06104c2b0ad7fea3cf1520d25
Tallen258 <112665786+Tallen258@users.noreply.github.com>
2022-09-21 16:15:04 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 415;
+int A = 830;
+int b= 466;
+int B= 932;
+int c =262 ;
+int C =523;
+int d = 277;
+int D =554;
+int e =311;
+int E =622;
+int f = 349;
+int g= 369;
+int G =740;
+int WholeNote= 5000;
+Console.Beep(G,  WholeNote/2) ;
+Console.Clear ();
+Console.Beep(E, WholeNote/2);
+Console.Beep(D, WholeNote/8);
+Console.Beep( E, WholeNote/8);
+Console.Beep(D, WholeNote/2);
+Console.Beep(C, WholeNote/8);
+Console.Beep(D, WholeNote/8);
+Console.Beep(c, WholeNote/8);
+Console.Beep( E, WholeNote/4 );
+Console.Beep(c, WholeNote/4);
+Console.Beep(b, WholeNote/8);
+Console.Beep(c, WholeNote/8);
+Console.Beep(b, WholeNote/2);
+Console.Beep(a, WholeNote/8);
+Console.Beep(b, WholeNote/8);
+Console.Beep(a, WholeNote/8);
+Console.Beep(D, WholeNote/8);
+Console.Beep(a, WholeNote/8);
+Console.Beep(g, WholeNote/8);
+Console.Beep(a, WholeNote/8);
+Console.Beep(g, WholeNote/8);
+Console.Beep(f, WholeNote/8);
+Console.Beep(g, WholeNote/8);
+Console.Beep(f, WholeNote/3);
+Console.Beep(e, WholeNote/3);
+Console.Beep(e, WholeNote/8);
+Console.Beep(f, WholeNote/8);
+Console.Beep(g, WholeNote/8);
+Console.Beep(a, WholeNote/8);
+Console.Beep(e, WholeNote/8);
+Console.Beep(d, WholeNote/8);
+Console.Beep(e, WholeNote/8);
+Console.Beep(d, WholeNote/8);
+Console.Beep(c, WholeNote/8);
+Console.Beep(d, WholeNote/8);
+Console.Beep(c, WholeNote/3);
+Console.Beep(b/2, WholeNote/3);
+
+
+
+
+Console.WriteLine("please work");
